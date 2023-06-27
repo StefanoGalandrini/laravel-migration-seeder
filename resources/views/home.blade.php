ef85f20d9be4f6751f6fcc14ebe52ed76e53e08c
StefanoGalandrini <stefe68@gmail.com>
2023-06-27 18:49:20 +0200

    Fix code and style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel Trains</title>
     @vite('resources/js/app.js')
 
     <style>
@@ -16,38 +16,39 @@
 </head>
 
 <body>
-    <div class="container">
+    <div class="wrapper">
         <h1 class="title">orario treni del {{ \Carbon\Carbon::now()->format('d-m-Y') }}</h1>
 
-        <div class="row header">
-            <div class="element">Azienda</div>
-            <div class="element">Stazione di Partenza</div>
-            <div class="element">Stazione di Arrivo</div>
-            <div class="element">Orario di Partenza</div>
-            <div class="element">Orario di Arrivo</div>
-            <div class="element">Durata</div>
-            <div class="element">Codice Treno</div>
-            <div class="element">Numero Carrozze</div>
-            <div class="element">In Orario</div>
-            <div class="element">Cancellato</div>
-        </div>
-
-        @foreach ($trains as $train)
-            <div class="row">
-                <div class="element">{{ $train->company }}</div>
-                <div class="element">{{ $train->departure_station }}</div>
-                <div class="element">{{ $train->arrival_station }}</div>
-                <div class="element">{{ $train->departure_time }}</div>
-                <div class="element">{{ $train->arrival_time }}</div>
-                <div class="element">{{ $train->duration }}</div>
-                <div class="element">{{ $train->train_code }}</div>
-                <div class="element">{{ $train->carriages_number }}</div>
-                <div class="element">{{ $train->delay }}</div>
-                <div class="element">{{ $train->cancelled ? 'Sì' : 'No' }}</div>
+        <div class="item">
+            <div class="data-item header">
+                <div class="element medium">Azienda</div>
+                <div class="element large">Stazione di Partenza</div>
+                <div class="element large">Stazione di Arrivo</div>
+                <div class="element small">Partenza</div>
+                <div class="element small">Arrivo</div>
+                <div class="element small">Durata</div>
+                <div class="element small">Treno n.</div>
+                <div class="element small">Carrozze</div>
+                <div class="element small">Ritardo</div>
+                <div class="element small">Cancellato</div>
             </div>
-        @endforeach
 
-    </div>
+            @foreach ($trains as $train)
+                <div class="data-item">
+                    <div class="element medium">{{ $train->company }}</div>
+                    <div class="element large">{{ $train->departure_station }}</div>
+                    <div class="element large">{{ $train->arrival_station }}</div>
+                    <div class="element small">{{ \Carbon\Carbon::parse($train->departure_time)->format('H:i') }}</div>
+                    <div class="element small">{{ \Carbon\Carbon::parse($train->arrival_time)->format('H:i') }}</div>
+                    <div class="element small">{{ \Carbon\Carbon::parse($train->duration)->format('H:i') }}</div>
+                    <div class="element small">{{ $train->train_code }}</div>
+                    <div class="element small">{{ $train->carriages_number }}</div>
+                    <div class="element small">{{ $train->delay }}</div>
+                    <div class="element small">{{ $train->cancelled ? 'Sì' : 'No' }}</div>
+                </div>
+            @endforeach
+
+        </div>
 
 </body>
 
